0b183bc3a771a5460f3a9e131b2d6a3404c3f5b7
Fino59 <slipdan@hotmail.com>
2024-11-08 18:18:40 -0300

    feat: Create Button component

---
+++ src/Button.tsx
@@ -0,0 +1,14 @@
+import { ReactNode } from "react";
+
+const Button = ({ disabled, children, onClick }: { disabled: boolean, children: ReactNode, onClick: () => void }) => {
+  return (
+    <button 
+      style={{ backgroundColor: disabled ? 'red' : 'black' }}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  )
+}
+
+export default Button;
