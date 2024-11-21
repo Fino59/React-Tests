fb14d82e92de35ff54ce14917a5048a8c651919f
Fino59 <slipdan@hotmail.com>
2024-11-21 19:19:57 -0300

    fix: Correct styles

--- src/Button.tsx
+++ src/Button.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 const Button = ({ disabled, children, onClick }: { disabled: boolean, children: ReactNode, onClick: () => void }) => {
   return (
     <button 
-      style={{ backgroundColor: disabled ? 'red' : 'black' }}
+      style={{ backgroundColor: disabled ? 'red' : 'black', padding: '10px', borderRadius: '6px', color: 'white' }}
       onClick={onClick}
     >
       {children}
